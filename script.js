e30dbba1820fcb65ed65e1bf4bcf52240a408f88
Nurdoğan Bahadır <nurdoganbahadirr@gmail.com>
2024-07-23 16:16:10 +0300

    mini proje started

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//!Example 1
 // index.html dosyası oluşturun ve yukarıdaki gibi dört p elemanı koyun: Etiket adını kullanarak document.querySelector(tagname) ile ilk paragrafı alın
 // const allParagraph = document.querySelector("p");
 // console.log(allParagraph);
@@ -13,12 +14,42 @@ const allParagraphTwo = document.querySelectorAll("p");
 // });
 
 // nodeList içinde dolaşın ve her paragrafın metin içeriğini alın
-for (let i = 0; i < allParagraphTwo.length; i++) {
-  console.log(allParagraphTwo[i]);
-}
+// for (let i = 0; i < allParagraphTwo.length; i++) {
+//   console.log(allParagraphTwo[i]);
+// }
 // Dördüncü paragrafta metin içeriği olarak Fourth Paragraph yerleştirin.
-allParagraphTwo[3].textContent = "Fourth Paragraph";
-console.log(allParagraphTwo[3]);
+// allParagraphTwo[3].textContent = "Fourth Paragraph";
+// console.log(allParagraphTwo[3]);
+
 // Farklı özellik ayarlama yöntemlerini kullanarak tüm paragraflar için id ve sınıf özelliklerini ayarlayın
-allParagraphTwo[3].className = "pFour";
-allParagraphTwo[0].id = "pOne";
+// allParagraphTwo[3].className = "pFour";
+// allParagraphTwo[0].id = "pOne";
+//!Example 2
+// JavaScript kullanarak her paragrafın stilini değiştirin (örn. renk, arka plan, sınır, yazı tipi boyutu, yazı tipi ailesi)
+// allParagraphTwo[2].style.backgroundColor = "red";
+// allParagraphTwo[2].style.color = "white";
+// allParagraphTwo[2].style.fontSize = "24px";
+
+// Tüm paragrafları seçin ve her elemanın içinde dolaşın ve ilk ve üçüncü paragrafı yeşil, ikinci ve dördüncü paragrafı kırmızı renkte verin
+// for (let i = 0; i < allParagraphTwo.length; i++) {
+//   if (i == 0 || i == 2) {
+//     allParagraphTwo[0].style.backgroundColor = "green";
+//     allParagraphTwo[2].style.backgroundColor = "green";
+//   } else {
+//     allParagraphTwo[1].style.backgroundColor = "red";
+//     allParagraphTwo[3].style.backgroundColor = "red";
+//   }
+// }
+
+// Her paragrafın içeriğini, id'sini ve sınıfını ayarlayın
+// for (let i = 0; i < allParagraphTwo.length; i++) {
+//   allParagraphTwo[i].className = "mardatone";
+//   console.log(allParagraphTwo);
+// }
+
+//! Mini Proje
+//? Yılın rengi her 1 saniyede değişiyor
+// Tarih ve saat arka plan rengi saniyede bir değişiyor
+// Tamamlanan meydan okuma arka planı yeşil
+// Devam eden meydan okuma arka planı sarı
+// Yaklaşan meydan okumaların arka planı kırmızı
